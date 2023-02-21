8eb7797c10b98f1f6e4e2d367196f5a933050657
Dmitry Safin <developersafin@gmail.com>
2023-02-21 14:28:46 +0500

    Fix bugs

--- NotesApp/NoteModel.swift
+++ NotesApp/NoteModel.swift
@@ -17,8 +17,20 @@ class Notes {
     let defaults = UserDefaults.standard
     static let shared = Notes()
     
-    var notes: [NoteModel]  {
-        
+    let note = NoteModel(note: "Description", noteDetails: """
+                                     Приложение 'Заметки'.
+                             
+                              Данное приложение состоит из двух экранов.
+                             
+                              На первом экране отображается список всех созданных заметок. При необходимости их можно удалять свайпом влево. А так же можно добавить новую заметку нажав "+". При клике на ячейку вы перейдете на экран редактирования заметки.
+                             
+                              На втором экране есть возможность вписать заголовок заметки и добавить более подробное описание. С TextView клавиатура убирается свайпом вниз. Заметка сохраняется нажатием по кнопке "Save".
+                             
+                              Все заметки сохраняются в памяти вашего телефона. Наслаждайтесь!
+                             """)
+    
+    var notes: [NoteModel] {
+                
         get {
             if let data = defaults.value(forKey: "notes") as? Data {
                 return try! PropertyListDecoder().decode([NoteModel].self, from: data)
@@ -31,6 +43,12 @@ class Notes {
             if let data = try? PropertyListEncoder().encode(newValue) {
                 defaults.set(data, forKey: "notes")
             }
+        }
+    }
+    
+    init() {
+        if notes.isEmpty {
+            notes.append(note)
         }
     }
     
